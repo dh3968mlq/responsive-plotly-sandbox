c8cb04f139b6abea651a56021eae26ee851be247
David Harris <dharris3968@hotmail.co.uk>
2024-04-01 19:40:49 +0100

    Use ag-grid and line plot

--- app.py
+++ app.py
@@ -2,19 +2,38 @@
 # Import packages
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import dash_bootstrap_components as dbc
+import dash_ag_grid as dag
 import pandas as pd
 import plotly.express as px
 
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+df = px.data.gapminder()
+df["lifeExp"] = df["lifeExp"].round(1)
+df["GDP per person"] = df["gdpPercap"].round(0)
+df = df.drop(columns="gdpPercap")
+g7 = {"CAN","FRA","DEU","ITA","JPN","GBR","USA"}
+df7 = df[df["iso_alpha"].isin(g7)]
 
 # Initialize the app
 app = Dash(__name__, 
     external_stylesheets=[dbc.themes.BOOTSTRAP,
-                          'https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css'
+                          #'https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css',
 ])
 
 # App layout
+columnDefs = [ {'field':col} for col in df.columns]
+
+grid = dag.AgGrid(
+    id="getting-started-filter",
+    rowData=df.to_dict("records"),
+    columnDefs=columnDefs,
+    className="ag-theme-alpine"
+)
+
+fig = px.line(df7, x="year", y="GDP per person", color="country", markers=True,
+             title="GDP per person for G7 countries")
+
 app.layout = html.Div([
         dbc.Row([
             html.Div('Example Graph', className="text-center fs-3"),
@@ -28,15 +47,20 @@ app.layout = html.Div([
         dbc.Row(
             [
                 dbc.Col([
-                    dash_table.DataTable(
-                        data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'},
-                    )
+                    grid
+                    #dash_table.DataTable(
+                    #    data=df.to_dict('records'), 
+                    #    page_size=12, 
+                        #style_table={'overflowX': 'auto'},
+                        #className="dbc",
+                    #)
                     ],
                     md=6,
                     id='col-datatable',
                 ),
                 dbc.Col([
-                    dcc.Graph(figure={}, id='fig-barchart')
+                    #dcc.Graph(figure={}, id='fig-barchart')
+                    dcc.Graph(figure=fig),
                     ],
                     id='col-graph',
                     md=6,
